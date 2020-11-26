f34053e4cef3db4fb8f02251d69483c735f6789c
zhaoxuyang03 <zhaoxuyang03@baidu.com>
2020-11-26 12:47:10 +0800

    【Sprint3.3.0】【Beetl】内存管理-减少Collection扩容开销

--- src/main/java/org/beetl/core/runtime/IBeetlMemoryManager.java
+++ src/main/java/org/beetl/core/runtime/IBeetlMemoryManager.java
@@ -1,7 +1,64 @@
 package org.beetl.core.runtime;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public interface IBeetlMemoryManager {
 
+    /**
+     * 创建数组实例
+     *
+     * @param type   类型
+     * @param minLen 长度
+     * @return {@param type} 类型的数组实例
+     */
     Object newUnpaddedArray(Class<?> type, int minLen);
+
+    /**
+     * 获取一个 {@link Map} 的实例
+     *
+     * @return Map 实例，如果池中实例不够，则会创建一个
+     */
+    Map map();
+
+    /**
+     * 将一个 {@link Map} 实例放入池中
+     *
+     * @param garbage 应该被回收的实例
+     * @return true 表示放入成功，false 表示放入失败
+     */
+    boolean recoveryMap(Map garbage);
+
+    /**
+     * 获取一个 {@link Set} 的实例
+     *
+     * @return Set 实例，如果池中实例不够，则会创建一个
+     */
+    Set set();
+
+    /**
+     * 将一个 {@link Set} 实例放入池中
+     *
+     * @param garbage 应该被回收的实例
+     * @return true 表示放入成功，false 表示放入失败
+     */
+    boolean recoverySet(Set garbage);
+
+    /**
+     * 获取一个 {@link List} 的实例
+     *
+     * @return List 实例，如果池中实例不够，则会创建一个
+     */
+    List list();
+
+    /**
+     * 将一个 {@link List} 实例放入池中
+     *
+     * @param garbage 应该被回收的实例
+     * @return true 表示放入成功，false 表示放入失败
+     */
+    boolean recoveryList(List garbage);
+
 
 }
--- src/main/java/org/beetl/core/runtime/impl/DefaultBeetlMemoryManager.java
+++ src/main/java/org/beetl/core/runtime/impl/DefaultBeetlMemoryManager.java
@@ -1,17 +1,30 @@
 package org.beetl.core.runtime.impl;
 
+import org.beetl.android.util.LruCache;
 import org.beetl.core.runtime.IBeetlMemoryManager;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import sun.misc.Unsafe;
 
+/**
+ * 通过维护 Collection 的引用，来避免重复创建 Collection 的实例
+ * 当 Collection 的容量较大时，MemoryManager的优势开始凸显，减少了 Collection 扩容的开销
+ */
 public class DefaultBeetlMemoryManager implements IBeetlMemoryManager {
 
     /** 单例模式 - 不可获取实例 */
     private DefaultBeetlMemoryManager() {
+        init();
     }
 
     /** 单例模式 - 通过内部类初始化，保证实例的线程安全 */
@@ -29,6 +42,64 @@ public class DefaultBeetlMemoryManager implements IBeetlMemoryManager {
         return Holder.INSTANCE;
     }
 
+    private static final int MAP_POOL_MAX_SIZE = 200;
+    private static final int LIST_POOL_MAX_SIZE = 200;
+    private static final int SET_POOL_MAX_SIZE = 200;
+    private List<Map> mapPool;
+    private List<List> listPool;
+    private List<Set> setPool;
+
+    private void init() {
+        mapPool = new ArrayList<>(MAP_POOL_MAX_SIZE);
+        listPool = new ArrayList<>(LIST_POOL_MAX_SIZE);
+        setPool = new ArrayList<>(SET_POOL_MAX_SIZE);
+    }
+
+    @Override
+    public Map map() {
+        return mapPool.size() == 0 ? new HashMap() : mapPool.remove(0);
+    }
+
+    @Override
+    public boolean recoveryMap(Map garbage) {
+        if (garbage == null) {
+            return false;
+        }
+        garbage.clear();
+        mapPool.add(garbage);
+        return true;
+    }
+
+    @Override
+    public Set set() {
+        return setPool.size() == 0 ? new HashSet() : setPool.remove(0);
+    }
+
+    @Override
+    public boolean recoverySet(Set garbage) {
+        if (garbage == null) {
+            return false;
+        }
+        garbage.clear();
+        setPool.add(garbage);
+        return true;
+    }
+
+    @Override
+    public List list() {
+        return listPool.size() == 0 ? new ArrayList() : listPool.remove(0);
+    }
+
+    @Override
+    public boolean recoveryList(List garbage) {
+        if (garbage == null) {
+            return false;
+        }
+        garbage.clear();
+        listPool.add(garbage);
+        return true;
+    }
+
     @Override
     public Object newUnpaddedArray(Class<?> kind, int length) {
         return Array.newInstance(kind, length);
@@ -39,6 +110,9 @@ public class DefaultBeetlMemoryManager implements IBeetlMemoryManager {
 //        return null;
     }
 
+    /** 通过反射获取的 Unsafe 类的实例 */
+    private static Unsafe unsafe;
+
     /**
      * 通过反射获取 Unsafe 类的实例
      *
@@ -46,13 +120,15 @@ public class DefaultBeetlMemoryManager implements IBeetlMemoryManager {
      */
     @Nullable
     public static Unsafe getUnsafe() {
-        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
-        unsafeField.setAccessible(true);
-        try {
-            return (Unsafe) unsafeField.get(null);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            return null;
+        if (unsafe == null) {
+            Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+            unsafeField.setAccessible(true);
+            try {
+                unsafe = (Unsafe) unsafeField.get(null);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
         }
+        return unsafe;
     }
 }
