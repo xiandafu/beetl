99c296719153354fd2ee38793be88cd39abb873e
lijiazhi <lijiazhi@pinefield-inc.com>
2022-09-28 00:01:14 +0800

    refine doc

--- beetl/src/main/java/org/beetl/core/parser/BeetlLexer.java
+++ beetl/src/main/java/org/beetl/core/parser/BeetlLexer.java
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class BeetlLexer extends Lexer {
-//	static { RuntimeMetaData.checkVersion("4.9.2", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.9.2", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
